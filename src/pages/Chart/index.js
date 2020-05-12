8934b9f467eb341446ff46652f321e24ee753aa6
Luis Henrique <42474086+HenriqueAnnechino@users.noreply.github.com>
2020-05-12 10:44:28 -0400

    up

--- src/pages/Chart/index.js
+++ src/pages/Chart/index.js
@@ -1,9 +1,10 @@
-//colocar tabela abaixo do grafico!!!
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 //import { format,addHours } from 'date-fns'
 import { Line } from 'react-chartjs-2';
+import * as ReactBootStrap from "react-bootstrap";
+import { Form, FormGroup, Label, Input, Button} from 'reactstrap';
+import './index.css';
 
 //import Header from '../../components/Header';
 
@@ -11,11 +12,19 @@ export default function Chart() {
 
 	var [chartData, setChartData] = useState({});
 	var [registros, setRegistros] = useState();
+	var [rows, setRows] = useState({
+		VRow: [],
+	});
+	var [displayX, setDisplayX] = useState(false);
 	const [periodo, setPeriodo] = useState('dia');
 	const [data, setData] = useState({
 		x: [],
 		y: []
-	})
+	});
+	const [BEperiodo, setBEperiodo] = useState({
+        inicio: '',
+        fim: ''
+    })
 
 	//Esse useEffect vai ser somente para recupar os dados do back
 	useEffect(() => {
@@ -23,7 +32,7 @@ export default function Chart() {
 		//console.log('Periodo selecionado -> ' + periodo)
 		async function LoadData() {
 			//console.log('Inicio da função para pegar os dados do back')
-			let response = await axios.get(`http://localhost:5000/dados_${periodo === 'dia' ? 'da_' : ''}precipitacao?periodo=${periodo}`)
+			let response = await axios.get(`http://localhost:5000/dados_${periodo === 'dia'|| periodo === 'atual' ? 'da_' : ''}precipitacao?periodo=${periodo}`)
 				.then(res => {
 					//console.log('Back retornou uma resposta')
 					return res.data.Precipitacao;
@@ -42,9 +51,11 @@ export default function Chart() {
 	//Vai preparar os dados
 	useEffect(() => {
 		async function PrepararDados() {
-			var xdata = [], ydata = [];
+			var xdata = [], ydata = [], Vdata = [];
 			var count = 0, count2, X, temp;
-			if (periodo === 'dia') {
+			setDisplayX(true);
+			if (periodo === 'dia' || periodo === 'atual') {
+				displayX = true;
 				//tratamento para os dados do dia
 				await registros.map((registro) => {
 					xdata.push(registro.createdAt);
@@ -63,93 +74,18 @@ export default function Chart() {
 						temp = ydata[count2];
 						ydata[count2] = ydata[count];
 						ydata[count] = temp;
+						Vdata[count] = {data: xdata[count], precipitacao: ydata[count]};
+						Vdata[count2] = {data: xdata[count2], precipitacao: ydata[count2]};
 						count++;
 						count2--;
 					}
-				} 
+				}
 				setData({
 					x: xdata,
 					y: ydata
 				})
-			}
-			else if (periodo === 'semana') {
-				//tratamento para os dados da semana
-				await registros.map((registro) => {
-					xdata.push(registro.data);
-					//let dataCerta = addHours(new Date(registro.data), 4);
-					//xdata.push(`${format(new Date(registro.data), 'yyyy/mm/dd')}`);
-					ydata.push(registro.precipitacao);
-					return; 
-				})
-				if(xdata[0]){
-					count2 = xdata.length -1;
-					while(count < xdata.length/2){
-						X = JSON.stringify(xdata[count]);
-						temp = JSON.stringify(xdata[count2]);
-						xdata[count] = temp.slice(9,11) + temp.slice(5,9) + temp.slice(1,5);
-						xdata[count2] = X.slice(9,11) + X.slice(5,9) + X.slice(1,5);
-						temp = ydata[count2];
-						ydata[count2] = ydata[count];
-						ydata[count] = temp;
-						count++;
-						count2--;
-					}
-				} 
-				setData({
-					x: xdata,
-					y: ydata
-				})
-			}
-			else if (periodo === 'quinzena') {
-				//tratamento para os dados da quinzena
-				await registros.map((registro) => {
-					xdata.push(registro.data);
-					ydata.push(registro.precipitacao);
-					return; 
-				})
-				if(xdata[0]){
-					count2 = xdata.length -1;
-					while(count < xdata.length/2){
-						X = JSON.stringify(xdata[count]);
-						temp = JSON.stringify(xdata[count2]);
-						xdata[count] = temp.slice(9,11) + temp.slice(5,9) + temp.slice(1,5);
-						xdata[count2] = X.slice(9,11) + X.slice(5,9) + X.slice(1,5);
-						temp = ydata[count2];
-						ydata[count2] = ydata[count];
-						ydata[count] = temp;
-						count++;
-						count2--;
-					}
-				} 
-				setData({
-					x: xdata,
-					y: ydata
-				})
-			}
-			else if (periodo === 'mes') {
-				//tratamento para os dados do mes
-				await registros.map((registro) => {
-					xdata.push(registro.data);
-					ydata.push(registro.precipitacao);
-					return; 
-				})
-				if(xdata[0]){
-					count2 = xdata.length -1;
-					while(count < xdata.length/2){
-						X = JSON.stringify(xdata[count]);
-						temp = JSON.stringify(xdata[count2]);
-						xdata[count] = temp.slice(9,11) + temp.slice(5,9) + temp.slice(1,5);
-						xdata[count2] = X.slice(9,11) + X.slice(5,9) + X.slice(1,5);
-						temp = ydata[count2];
-						ydata[count2] = ydata[count];
-						ydata[count] = temp;
-						count++;
-						count2--;
-					}
-				} 
-				setData({
-					x: xdata,
-					y: ydata
+				setRows({
+					VRow: Vdata
 				})
 			}
 			else if (periodo === 'ano') {
@@ -169,6 +105,8 @@ export default function Chart() {
 						temp = ydata[count2];
 						ydata[count2] = ydata[count];
 						ydata[count] = temp;
+						Vdata[count] = {data: xdata[count], precipitacao: ydata[count]};
+						Vdata[count2] = {data: xdata[count2], precipitacao: ydata[count2]};
 						count++;
 						count2--;
 					}
@@ -177,6 +115,46 @@ export default function Chart() {
 					x: xdata,
 					y: ydata
 				})
+				setRows({
+					VRow: Vdata
+				})
+			}
+			else{
+				//tratamento para os dados da semana, quinzena, mes e input
+				await registros.map((registro) => {
+					xdata.push(registro.data);
+					//let dataCerta = addHours(new Date(registro.data), 4);
+					//xdata.push(`${format(new Date(registro.data), 'yyyy/mm/dd')}`);
+					ydata.push(registro.precipitacao);
+					return; 
+				})
+				if(xdata[0]){
+					count2 = xdata.length -1;
+					while(count < xdata.length/2){
+						X = JSON.stringify(xdata[count]);
+						temp = JSON.stringify(xdata[count2]);
+						xdata[count] = temp.slice(9,11) + temp.slice(5,9) + temp.slice(1,5);
+						xdata[count2] = X.slice(9,11) + X.slice(5,9) + X.slice(1,5);
+						temp = ydata[count2];
+						ydata[count2] = ydata[count];
+						ydata[count] = temp;
+						Vdata[count] = {data: xdata[count], precipitacao: ydata[count]};
+						Vdata[count2] = {data: xdata[count2], precipitacao: ydata[count2]};
+						count++;
+						count2--;
+					}
+				} 
+				setData({
+					x: xdata,
+					y: ydata
+				})
+				setRows({
+					VRow: Vdata
+				})
+				if (periodo === 'semana' || periodo === 'quinzena' || periodo === 'mes')
+					setDisplayX(true);
+				else
+					setDisplayX(false);
 			}
 		}
 
@@ -189,12 +167,11 @@ export default function Chart() {
 		}
 	}, [registros, periodo]) //ele só vai ser chamado novamente se o periodo ou os registros forem alterados
 	
-
 	//vai montar o gráfico
 	useEffect(() => {
 		if (data.x.length > 0 && data.y.length > 0) {
 			setChartData({
-			labels: data.x,
+				labels: data.x,
 				datasets: [
 					{
 						label: 'nivel de precipitação',
@@ -208,22 +185,183 @@ export default function Chart() {
 			})
 		}else{
 			//A primeira vez ele vai cair aqui, daí nao vai executar nada
-			console.log('vazio')
+			console.log('')
 		}
+
 	}, [data]) //ele só vai se chamado novamente quando alterar alguma coisa dentro da variavel 'data
+
+
+	function renderData(Data, index) {
+		//console.log(rows.VRow);
+		return(
+			<tr key={index}>
+				<td>{Data.data}</td>
+				<td>{Data.precipitacao}</td>
+			</tr>
+		)
+	}
+
+	function preparaPeriodo(){
+		//console.log(typeof(BEperiodo.inicio + BEperiodo.fim));
+		if(BEperiodo.inicio.length === 10 || BEperiodo.fim.length === 10){
+			var tempPeriodo = [], error=0;
+			if(BEperiodo.inicio[2]!=="/" || BEperiodo.inicio[5]!=="/" || BEperiodo.fim[2]!=="/" || BEperiodo.fim[5]!=="/"){
+				error=1;
+				alert("data invalida");
+			}
+			else {
+				var count = 0, count2 = 0;
+				while(count < 10){
+					if(count===2 || count===5)
+						count++;
+					tempPeriodo[count2] = parseInt(BEperiodo.inicio[count]);
+					tempPeriodo[count2 + 8] = parseInt(BEperiodo.fim[count]);
+					if(isNaN(tempPeriodo[count2])===false && isNaN(tempPeriodo[count2 + 8])===false){
+						count++;
+						count2++;
+					}
+					else{
+						error=1;
+						alert("data invalida");
+						break;
+					}
+				}
+				if(error===0){
+					tempPeriodo[0]=tempPeriodo[0] * 10 + tempPeriodo[1];
+					tempPeriodo[1]=tempPeriodo[2] * 10 + tempPeriodo[3];
+					tempPeriodo[2]=tempPeriodo[4] * 1000 + tempPeriodo[5] * 100 + tempPeriodo[6] * 10 + tempPeriodo[7];
+					tempPeriodo[3]="_";
+					tempPeriodo[4]=tempPeriodo[8] * 10 + tempPeriodo[9];
+					tempPeriodo[5]=tempPeriodo[10] * 10 + tempPeriodo[11];
+					tempPeriodo[6]=tempPeriodo[12] * 1000 + tempPeriodo[13] * 100 + tempPeriodo[14] * 10 + tempPeriodo[15];
+					
+					if(tempPeriodo[1]===1 || tempPeriodo[1]===3 || tempPeriodo[1]===5 || tempPeriodo[1]===7 || tempPeriodo[1]===8 || tempPeriodo[1]===10 || tempPeriodo[1]===12){
+						if(tempPeriodo[0]<1 || tempPeriodo[0]>31){
+							error=1;
+							alert("data invalida");
+						}
+					}
+					else if(tempPeriodo[1]===4 || tempPeriodo[1]===6 || tempPeriodo[1]===9 || tempPeriodo[1]===11){
+						if(tempPeriodo[0]<1 || tempPeriodo[0]>30){
+							error=1;
+							alert("data invalida");
+						}
+					}
+					else if(tempPeriodo[1]===2){
+						if((tempPeriodo[2]%4===0 && tempPeriodo[2]%100!==0) || tempPeriodo[2]%400===0){
+							if(tempPeriodo[0]<1 || tempPeriodo[0]>29){
+								error=1;
+								alert("data invalida");
+							}
+						}
+						else{
+							if(tempPeriodo[0]<1 || tempPeriodo[0]>28){
+								error=1;
+								alert("data invalida");
+							}
+						}
+					}
+					if(tempPeriodo[1]<1 || tempPeriodo[1]>12){
+						error=1;
+						alert("data invalida");
+					}
+
+					if(error===0){
+						if(tempPeriodo[5]===1 || tempPeriodo[5]===3 || tempPeriodo[5]===5 || tempPeriodo[5]===7 || tempPeriodo[5]===8 || tempPeriodo[5]===10 || tempPeriodo[5]===12){
+							if(tempPeriodo[4]<1 || tempPeriodo[4]>31){
+								error=1;
+								alert("data invalida");
+							}
+						}
+						else if(tempPeriodo[5]===4 || tempPeriodo[5]===6 || tempPeriodo[5]===9 || tempPeriodo[5]===11){
+							if(tempPeriodo[4]<1 || tempPeriodo[4]>30){
+								error=1;
+								alert("data invalida");
+							}
+						}
+						else if(tempPeriodo[5]===2){
+							if((tempPeriodo[6]%4===0 && tempPeriodo[6]%100!==0) || tempPeriodo[6]%400===0){
+								if(tempPeriodo[4]<1 || tempPeriodo[4]>29){
+									error=1;
+									alert("data invalida");
+								}
+							}
+							else{
+								if(tempPeriodo[4]<1 || tempPeriodo[4]>28){
+									error=1;
+									alert("data invalida");
+								}
+							}
+						}
+						if(tempPeriodo[5]<1 || tempPeriodo[5]>12){
+							error=1;
+							alert("data invalida");
+						}
+					}
+					if(error===0){
+						//checa se a data de inicio eh menor que a final
+						if(tempPeriodo[2] > tempPeriodo[6]){
+							error=1;
+							alert("data de inicio nao pode ser menor nem igual a data final");
+						}
+						else if(tempPeriodo[2] === tempPeriodo[6] && tempPeriodo[1] > tempPeriodo[5]){
+							error=1;
+							alert("data de inicio nao pode ser menor nem igual a data final");
+						}
+						else if((tempPeriodo[2] === tempPeriodo[6] && tempPeriodo[1] === tempPeriodo[5]) && tempPeriodo[0] > tempPeriodo[4]){
+							error=1;
+							alert("data de inicio nao pode ser menor nem igual a data final");
+						}
+						else{
+							count = 0;
+							var tempPeriodo2 = "";
+							while(count < 7){
+								if(tempPeriodo[count].toString().length === 1)
+									tempPeriodo2 += "0" + tempPeriodo[count].toString();
+								else
+									tempPeriodo2 += tempPeriodo[count].toString();
+								count++;
+							}
+							tempPeriodo2 = tempPeriodo2.slice(4,8) + "-" + tempPeriodo2.slice(2,4) + "-" + tempPeriodo2.slice(0,2) + "_" + tempPeriodo2.slice(14,18) + "-" + tempPeriodo2.slice(12,14) + "-" + tempPeriodo2.slice(10,12);
+
+							setPeriodo(tempPeriodo2);
+						}
+					}
+				}
+			}
+		}
+		else{
+			alert("Por Favor insira um periodo nesse formato inicio(dd/mm/yyyy) e fim(dd/mm/yyyy)");
+		}
+	}
 
 	return (
 		//style={{height: "500px", width: "500px"}}
 		<div className="App">
-			<h1>Grafico Precipitação</h1>
-			<div style={{float: "left", "margin-left": "5%"}}>
-				<button onClick={() => setPeriodo('dia')}>1D</button>
-				<button onClick={() => setPeriodo('semana')}>1S</button>
-				<button onClick={() => setPeriodo('quinzena')}>15D</button>
-				<button onClick={() => setPeriodo('mes')}>1M</button>
-				<button onClick={() => setPeriodo('ano')}>1A</button>
-			</div>
-			<div style={{height: "800px", width: "1100px"}}>
+			
+			<div style={{height: "660px", width: "1100px"}}>
+				<h1>Grafico Precipitação</h1>
+				
+				<div style={{float: "left", 'marginLeft': "5%"}}>
+					<button onClick={() => setPeriodo('atual')}>Atual</button>
+					<button onClick={() => setPeriodo('dia')}>1D</button>
+					<button onClick={() => setPeriodo('semana')}>1S</button>
+					<button onClick={() => setPeriodo('quinzena')}>15D</button>
+					<button onClick={() => setPeriodo('mes')}>1M</button>
+					<button onClick={() => setPeriodo('ano')}>1A</button>
+				</div>
+
+				<div style={{float: "right", 'marginRight': "5%"}}>
+					<Form>
+						<FormGroup>
+							<Label for="Periodo_inicial">Inicio</Label>
+							<Input type="text" value={BEperiodo.inicio} id="Periodo_inicial" onChange={e => setBEperiodo({ ...BEperiodo, inicio: e.target.value })} placeholder="dd/mm/yyyy" />
+							<Label for="Periodo_final">Fim</Label>
+							<Input type="text" value={BEperiodo.fim} id="Periodo_final" onChange={e => setBEperiodo({ ...BEperiodo, fim: e.target.value })} placeholder="dd/mm/yyyy" />
+							<Button color="primary" block onClick={preparaPeriodo}> Buscar </Button>
+						</FormGroup>
+					</Form>
+				</div>
 
 				{registros ?
 					<Line data={chartData} options={{
@@ -246,6 +384,9 @@ export default function Chart() {
 								{
 									gridLines: {
 										display: false
+									},
+									ticks: {
+										display: displayX
 									}
 								}
 							]
@@ -254,7 +395,19 @@ export default function Chart() {
 					:
 					<p>Carregando....</p>
 				}
-
+			</div>
+			<div className="container">
+				<ReactBootStrap.Table striped bordered hover>
+					<thead>
+						<tr>
+							<th>Data</th>
+							<th>Precipitação</th>
+						</tr>
+					</thead>
+					<tbody>
+						{rows.VRow.map(renderData)}
+					</tbody>
+				</ReactBootStrap.Table>
 			</div>
 		</div>
 	)
